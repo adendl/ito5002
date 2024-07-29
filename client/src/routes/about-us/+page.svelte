965d7e7d8ed8d4afc1e5a083fe4b5d7f2c288ed9
Oliver Smith <oliver.smith.mail@gmail.com>
2024-07-29 12:04:17 +1000

    cross merge about-us page and add navigation elemets

---
+++ client/src/routes/about-us/+page.svelte
@@ -0,0 +1,70 @@
+<a href="/login" type="button" class="btn-icon variant-filled mt-8 mx-8"
+    ><i class="fa-solid fa-angle-left"></i></a
+>
+<div class="about-us">
+    <h2>About Us</h2>
+    <p>
+        Community Power provides a simple way to book and reserve electric
+        vehicle (EV) chargers. We want to make it easier to use EVs to help
+        create a greener future.
+    </p>
+
+    <h3>Our Purpose and Goals</h3>
+    <p>
+        The primary goal of Community Power is to offer a reliable and
+        user-friendly platform for EV owners to locate and book charging
+        stations. We strive to:
+    </p>
+    <ul>
+        <li>Increase the convenience of finding and using EV chargers.</li>
+        <li>Promote the adoption of electric vehicles.</li>
+        <li>Support sustainable and eco-friendly transportation solutions.</li>
+    </ul>
+
+    <h3>Supporting UNDP Goals</h3>
+    <p>
+        CommunityPower is dedicated to the United Nations' Sustainable
+        Development Goals, aiming to ensure access to affordable, reliable,
+        sustainable, and modern energy for all.
+    </p>
+
+    <h3>Community Guidelines</h3>
+    <p>
+        CommunityPower is an inclusive platform meant for everyone. Please be
+        clear and polite when communicating with others, and keep engagements
+        positive and constructive. Ensure you keep your energy sharing
+        commitments and make use of our rating features to help others in the
+        community.
+    </p>
+
+    <h3>Privacy</h3>
+    <p>
+        We use third party services sources to help us find users locations. By
+        using Community Power, you agree to your location searches being shared
+        with a third party for the purposes of geocoding.
+    </p>
+</div>
+
+<style>
+    .about-us {
+        @apply p-8 max-w-3xl mx-auto;
+    }
+    .about-us h2 {
+        @apply text-2xl font-bold mb-4;
+    }
+    .about-us h3 {
+        @apply text-xl font-semibold mt-6 mb-3;
+    }
+    .about-us p {
+        @apply mb-4 leading-relaxed;
+    }
+    .about-us ul {
+        @apply list-disc pl-5;
+    }
+    .about-us li {
+        @apply mb-2;
+    }
+    .back-button {
+        @apply absolute top-0 left-0 p-2 text-white bg-blue-500 rounded-md;
+    }
+</style>
