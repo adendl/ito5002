f50f5c04ba393ff08d9e778259dff0a751a4f8d4
adendl <adendeleeuw@outlook.com>
2024-08-07 12:45:39 +1000

    update CI/CD

--- server/payment-service/payment-api.py
+++ server/payment-service/payment-api.py
@@ -1,35 +1,31 @@
+import os
 from flask import Flask, request, jsonify
 import stripe
-from dotenv import load_dotenv
-import os
 from flask_cors import CORS
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Initialize Flask app and enable CORS
 app = Flask(__name__)
 CORS(app)
 
-# Set Stripe API key
-stripe.api_key = os.getenv('SECRET_API')
+# Load environment variable directly if available, otherwise load from .env file
+if 'SECRET_API' in os.environ:
+    stripe.api_key = os.getenv('SECRET_API')
+else:
+    from dotenv import load_dotenv
+    load_dotenv()  # Load environment variables from .env file
+    stripe.api_key = os.getenv('SECRET_API')
 
 @app.route('/create-payment-intent', methods=['POST'])
 def create_payment_intent():
     try:
         data = request.get_json()
-        print("Request data:", data)  # Print the request data to the console
-
-        amount = data['amount']  # The amount to charge in cents
+        amount = data['amount']
         payment_intent = stripe.PaymentIntent.create(
             amount=amount,
             currency='aud',
             payment_method_types=['card']
         )
-        # Correctly access the client secret
         return jsonify({'clientSecret': payment_intent['client_secret']})
     except Exception as e:
-        print("Error:", str(e))  # Print any exceptions to the console
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
