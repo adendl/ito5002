9c855b0b37a7afc55555d8f1aba1583c9dae9263
Oliver Smith <oliver.smith.mail@gmail.com>
2024-08-10 16:35:18 +1000

    add backend trigger and frontent element to create notifications for request acceptance and refusal

--- client/src/lib/MessageFeed.svelte
+++ client/src/lib/MessageFeed.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     export let data;
     import { onMount } from "svelte";
 
@@ -6,12 +6,20 @@
     $: ({ supabase, session } = data);
 
     let messages = [];
+    let loaded = false;
+    import { ConicGradient } from "@skeletonlabs/skeleton";
+    import type { ConicStop } from "@skeletonlabs/skeleton";
+    const conicStops: ConicStop[] = [
+        { color: "transparent", start: 0, end: 25 },
+        { color: "rgb(var(--color-primary-500))", start: 75, end: 100 },
+    ];
 
     onMount(async () => {
         await getMessages();
     });
 
     async function getMessages() {
+        loaded = false;
         const { data: messageData, error } = await supabase
             .from("notifications")
             .select("*")
@@ -20,51 +28,60 @@
 
         if (error) {
             console.error("error", error);
-            return;
         } else {
             messages = messageData;
         }
+        loaded = true;
     }
 
     function resolveMessage(messageText) {
         if (messageText === "booking_request") {
-            return "You have a new request for your charger on "
+            return "You have a new request for your charger on ";
+        } else if (messageText === "request_accepted") {
+            return "Your request was accepted for a charger on ";
+        } else if (messageText === "request_rejected") {
+            return "Your request was rejected for a charger on ";
         } else {
-            return 
+            return "Error parsing message for event on ";
         }
     }
 
     function prettyDate(date) {
         // format should be 10 August 2021
-        return new Date(date).toLocaleDateString('en-GB', {
-            day: 'numeric',
-            month: 'long',
-            year: 'numeric'
+        return new Date(date).toLocaleDateString("en-GB", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
         });
     }
 
     function prettyTimestamp(timestamp) {
         // format should be 2021-08-10 12:00:00 in the user's timezone
-        return new Date(timestamp).toLocaleString('en-GB', {
-            day: 'numeric',
-            month: 'long',
-            year: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric',
-            second: 'numeric'
+        return new Date(timestamp).toLocaleString("en-GB", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
         });
     }
-
 </script>
 
-{#if messages.length === 0}
+{#if !loaded}
+    <div class="w-full h-full flex items-center justify-center">
+        <ConicGradient stops={conicStops} size="100px" />
+    </div>
+{:else if messages.length === 0}
     <div class="w-full h-full flex items-center justify-center">
         <p>No notifications to display</p>
     </div>
 {:else}
     {#each messages as message}
         <div class="card p-4 m-4 card-hover">
-            <p class="text-sm text-gray-400">{prettyTimestamp(message.timestamp)}</p>
+            <p class="text-sm text-gray-400">
+                {prettyTimestamp(message.timestamp)}
+            </p>
             <p>{resolveMessage(message.message)} {prettyDate(message.date)}.</p>
         </div>
     {/each}
