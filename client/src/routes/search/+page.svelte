1addf149af6a26c3257a031e7c0c92707c8f3625
Oliver Smith <oliver.smith.mail@gmail.com>
2024-08-11 22:10:09 +1000

    re implement filtering of search results on search page

--- client/src/routes/search/+page.svelte
+++ client/src/routes/search/+page.svelte
@@ -6,8 +6,14 @@
     import { onMount } from "svelte";
     import ListingMarker from "$lib/ListingMarker.svelte";
     import GeocodeSearchbar from "$lib/GeocodeSearchbar.svelte";
-    import { ConicGradient, getToastStore } from "@skeletonlabs/skeleton";
-    import type { ConicStop } from "@skeletonlabs/skeleton";
+    import {
+        ConicGradient,
+        getToastStore,
+        ListBox,
+        ListBoxItem,
+        popup,
+    } from "@skeletonlabs/skeleton";
+    import type { ConicStop, PopupSettings } from "@skeletonlabs/skeleton";
     const conicStops: ConicStop[] = [
         { color: "transparent", start: 0, end: 25 },
         { color: "rgb(var(--color-primary-500))", start: 75, end: 100 },
@@ -20,7 +26,17 @@
     let addressSuburb = "";
     let userHomePlace;
     let userWorkPlace;
+    let filteredChargerType;
+    let blankMessage = "Enter a address to get started";
     $: addressPoint, getNearbyListings();
+    $: filteredChargerType, getNearbyListings();
+
+    const popupCombobox: PopupSettings = {
+        event: "click",
+        target: "popupCombobox",
+        placement: "bottom",
+        closeQuery: ".listbox-item",
+    };
 
     onMount(async () => {
         getUser();
@@ -58,9 +74,16 @@
         console.log("getNearbyListings", addressPoint);
         if (!addressPoint || addressPoint === "") return;
         loaded = false;
+        blankMessage = "No results found"; 
+        mapComponent = null;
+        averageCoordinate = null;
+        zoomLevel = null;
         const { data, error } = await supabase.rpc("get_listings_near", {
             query_location: addressPoint,
             limit_count: 100,
+            charger_type_param: filteredChargerType
+                ? filteredChargerType
+                : null,
         });
         if (error) {
             console.error("error", error);
@@ -68,8 +91,17 @@
                 message: "Error fetching listings",
                 background: "variant-filled-error",
             });
+            loaded = true;
             return;
         } else {
+            if (data.length === 0) {
+                toastStore.trigger({
+                    message: "No results found",
+                    background: "variant-filled-error",
+                });
+                loaded = true;
+                return;
+            }
             listings = data;
             averageCoordinate = getAverageCoordinate(listings);
             zoomLevel = getZoomLevel(listings);
@@ -98,9 +130,42 @@
         query={addressString}
         n={1}
     />
-    <button type="button" class="btn m-2 variant-filled" disabled>
+    <button
+        type="button"
+        class="btn m-2 variant-filled"
+        use:popup={popupCombobox}
+    >
         <i class="fa-solid fa-filter"></i>
     </button>
+    <div class="card w-48 shadow-xl py-2 z-50" data-popup="popupCombobox">
+        <ListBox rounded="rounded-none">
+            <ListBoxItem bind:group={filteredChargerType} value=""
+                >All</ListBoxItem
+            >
+            <ListBoxItem bind:group={filteredChargerType} value="Mennekes"
+                >Mennekes</ListBoxItem
+            >
+            <ListBoxItem bind:group={filteredChargerType} value="Type 1"
+                >Type 1</ListBoxItem
+            >
+            <ListBoxItem bind:group={filteredChargerType} value="CHaDeMO"
+                >CHaDeMO</ListBoxItem
+            >
+            <ListBoxItem bind:group={filteredChargerType} value="CCS"
+                >CCS</ListBoxItem
+            >
+            <ListBoxItem bind:group={filteredChargerType} value="CCS2"
+                >CCS2</ListBoxItem
+            >
+            <ListBoxItem bind:group={filteredChargerType} value="Tesla"
+                >Tesla</ListBoxItem
+            >
+            <ListBoxItem bind:group={filteredChargerType} value="GB/T"
+                >GB/T</ListBoxItem
+            >
+        </ListBox>
+        <div class="arrow bg-surface-100-800-token" />
+    </div>
     <button
         class="btn variant-filled m-2"
         disabled={!userHomePlace}
@@ -151,7 +216,7 @@
 {:else}
     <div class="card p-4 h-[75vh] overflow-auto">
         <div class="flex justify-center items-center h-[90%]">
-            <h4 class="h4">Enter an address to get started</h4>
+            <h4 class="h4">{blankMessage}</h4>
         </div>
     </div>
 {/if}
